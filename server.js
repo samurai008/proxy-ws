6f3334c7615f3ce806507b727e33f7fda5bd100a
Nilabjo Sanyal <n.sanyal@hotmail.com>
2018-12-14 07:15:55 +0530

    refactor

--- server.js
+++ server.js
@@ -12,23 +12,6 @@ const INDEX = path.join(__dirname, 'index.html');
 const server = express()
   .use((req, res) => res.sendFile(INDEX) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
- 
-// const https = require('https');
-
-// https.createServer({
-// }, app)
-// .listen(PORT, function () {
-//   console.log('Example app listening on port 3000! Go to https://localhost:3000/')
-// })
-
-// app.get('/', function(req, res) {
-//   const mediaws = new WebSocket('ws://stocks.mnet.website');
-
-//   mediaws.on('message', (data) => {
-//     console.log('incoming', data);
-//   });
-//   res.sendFile(INDEX);
-// })
 
 const wss = new SocketServer({ server });
 const mediaws = new WebSocket('ws://stocks.mnet.website');
@@ -43,19 +26,3 @@ wss.on('connection', (ws) => {
   });
   ws.on('close', () => console.log('Client disconnected'));
 });
-
-// wss.clients.forEach((client) => {
-//   console.log('in wss client');
-//   const mediaws = new WebSocket('ws://stocks.mnet.website');
-//   mediaws.on('message', (data) => {
-//     client.send(data);
-//   });
-// });
-// setInterval(() => {
-//   wss.clients.forEach((client) => {
-//     mediaws.on('message', (data) => {
-//       client.send(data);
-//     });
-//     // client.send(new Date().toTimeString());
-//   });
-// }, 0);
